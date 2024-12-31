ad153aa326d7174bb2550277b21784e6630fc1a0
meleu <meleu.dev@gmail.com>
2024-12-31 16:14:32 -0300

    refactoring

--- src/list_command.sh
+++ src/list_command.sh
@@ -1,1 +1,13 @@
-list_command
+if [[ "${ARGS['--copy-url']}" == 1 ]]; then
+  pick_and_copy_url
+  return
+fi
+
+if [[ "${ARGS['--browser']}" == 1 ]]; then
+  pick_and_open_url
+  return
+fi
+
+[[ "${ARGS['--update']}" == 1 ]] && update_log_file
+
+print_uploaded_files
